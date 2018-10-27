8164b9562194a16517d456dbed8f33d665cb1ebe
ricavalcanti <ricavalcanti@bct.ect.ufrn.br>
2018-10-27 11:52:54 -0300

    using http and post data to SaIoT

--- httpRequestPython/usingPost.py
+++ httpRequestPython/usingPost.py
@@ -1,31 +1,71 @@
 import requests
 import json
+import time
 
-# pickup token
-jsonLogin = {"email": "ricardo@email.com",
-             "password": "12345678910", "serial": "SO261018LAB"}
+# parametros
+serial = "TEST02271018"
+email = "rodolfo@email.com"
+kVol = "sensorVolume1"
+kCond = "sensorCond1"
+kTemp = "sensorTemp1"
+jsonLogin = {"email": email,
+             "password": "12345678910", "serial": serial}
 urlLogin = 'http://api.saiot.ect.ufrn.br/v1/device/auth/login'
 urlCadastro = 'http://api.saiot.ect.ufrn.br/v1/device/manager/device'
-
+urlHist = 'http://api.saiot.ect.ufrn.br/v1/device/history/logs'
+broker = "api.saiot.ect.ufrn.br"
+# pickup token
 r = requests.post(urlLogin, json=jsonLogin)
 token = r.text
 print(token)
+# cadastro device
 jsonDevice = {"token":
               token,
               "data": {
                   "name": "SmartOrdenha",
-                  "serial": "SO261018LAB",
+                  "serial": serial,
+                  "controllers": [],
                   "sensors": [
                       {
-                          "key": "sensorVolume1",
-                          "tag": "Volume teteiras 01",
+                          "key": kVol,
+                          "tag": "Leite Produzido",
                           "unit": "Litros",
                           "type": "number"
+                      },
+                      {
+                          "key": kCond,
+                          "tag": "Condutância",
+                          "unit": "miliSiemens/cm",
+                          "type": "number"
+                      },
+                      {
+                          "key": kTemp,
+                          "tag": "Temperatura",
+                          "unit": "°C",
+                          "type": "number"
                       }
-                  ],
+                  ]
               }
               }
 
-retornoCadastro = requests.post(urlCadastro,json =jsonDevice)
-print(retornoCadastro.text)
+retornoCadastro = requests.post(urlCadastro, json=jsonDevice)
+print("Retorno do cadastro: " + retornoCadastro.text)
 
+
+def sendData(dVolume, dTemp, dCond,dateTime):
+    jsonReport = {
+        "token": token,
+        "data": [{
+            "data": [{
+                "serial": serial,
+                kVol: dVolume,
+                kCond : dCond,
+                kTemp: dTemp
+            }],
+            "dateTime": dateTime
+        }]
+    }
+    retornoData = requests.post(urlHist, json=jsonReport)
+    print(retornoData.text)
+
+sendData(20,28,5.3,"2018-10-26 09:51:52.220")
--- usingMqtt_Python/publishOnSaIoT.py
+++ usingMqtt_Python/publishOnSaIoT.py
@@ -1,19 +1,20 @@
 import requests
 import json
 import time
-import paho.mqtt.client as paho
 
 # parametros
-serial = "TEST271018"
+serial = "TEST02271018"
 email = "rodolfo@email.com"
+kVol = "sensorVolume1"
+kCond = "sensorCond1"
+kTemp = "sensorTemp1"
 jsonLogin = {"email": email,
              "password": "12345678910", "serial": serial}
 urlLogin = 'http://api.saiot.ect.ufrn.br/v1/device/auth/login'
 urlCadastro = 'http://api.saiot.ect.ufrn.br/v1/device/manager/device'
-urlHist = 'api.saiot.ect.ufrn.br/v1/device/history/logs'
+urlHist = 'http://api.saiot.ect.ufrn.br/v1/device/history/logs'
 broker = "api.saiot.ect.ufrn.br"
 # pickup token
-r = requests.post(urlLogin, json=jsonLogin)
 r = requests.post(urlLogin, json=jsonLogin)
 token = r.text
 print(token)
@@ -23,62 +24,48 @@ jsonDevice = {"token":
               "data": {
                   "name": "SmartOrdenha",
                   "serial": serial,
-                  "controllers": [
-                      {
-                          "key": "onOff",
-                          "class": "onoff",
-                          "tag": "testePublish"
-                      }
-                  ],
+                  "controllers": [],
                   "sensors": [
                       {
-                          "key": "sensorVolume1",
-                          "tag": "Volume teteiras 01",
+                          "key": kVol,
+                          "tag": "Leite Produzido",
                           "unit": "Litros",
                           "type": "number"
+                      },
+                      {
+                          "key": kCond,
+                          "tag": "Condutância",
+                          "unit": "miliSiemens/cm",
+                          "type": "number"
+                      },
+                      {
+                          "key": kTemp,
+                          "tag": "Temperatura",
+                          "unit": "°C",
+                          "type": "number"
                       }
-                  ],
+                  ]
               }
               }
 
 retornoCadastro = requests.post(urlCadastro, json=jsonDevice)
-print(retornoCadastro.text)
+print("Retorno do cadastro: " + retornoCadastro.text)
 
 
-# define callback
-
-
-def on_message(client, userdata, message):
-    time.sleep(1)
-    print("received message =", str(message.payload.decode("utf-8")))
-    print("message topic=", message.topic)
-    print("message qos=", message.qos)
-    print("message retain flag=", message.retain)
-
-
-client = paho.Client(serial, email, token)
-client.on_message = on_message
-print("connecting to broker ", broker)
-client.connect(broker, port=8000)
-print("subscribing ")
-print(client.subscribe(serial))
-print(client.subscribe((serial + "onOff")))  # subscribe
-#print("publishing ")
-jsonReport = {
-    "token": token,
-    "data": {
-        "serial": serial,
-        "key": "onOff",
-        "value": "1"
+def sendData(dVolume, dTemp, dCond,dateTime):
+    jsonReport = {
+        "token": token,
+        "data": [{
+            "data": [{
+                "serial": serial,
+                kVol: dVolume,
+                kCond : dCond,
+                kTemp: dTemp
+            }],
+            "dateTime": dateTime
+        }]
     }
-}
-print("Json report: " + json.dumps(jsonReport))
-client.publish("/control/put/me/", payload = json.dumps(jsonReport))  # publish
-jsonSensor = { "serial": serial, "key": "sensorVolume1", "value": 18 }
-time.sleep(4)
-client.loop_start()
-while 1:
-    print("In loop")
-    time.sleep(2)
-    client.publish("/history/post/logs/sensor/", json.dumps(jsonSensor))
-    #client.publish("/control/put/me/", json.dumps(jsonReport))  # publish
+    retornoData = requests.post(urlHist, json=jsonReport)
+    print(retornoData.text)
+
+sendData(20,28,5.3,"2018-10-26 09:51:52.220")
