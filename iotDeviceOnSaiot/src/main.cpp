12134514fde0b0e56f32f99e9d3632a412f8e76d
Ricardo <ricavalcanti@bct.ect.ufrn.br>
2019-05-24 10:12:21 -0300

    Testing update submodule

--- iotDeviceOnSaiot/src/main.cpp
+++ iotDeviceOnSaiot/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <SaIoTDeviceLib.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
